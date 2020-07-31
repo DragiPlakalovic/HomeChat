ae5b0dacc45eb3fd06fab2fd13963a125e6325de
DragiPlakalovic <55633123+DragiPlakalovic@users.noreply.github.com>
2020-07-30 22:36:32 -0400

    Full GUI made

--- code/simplechat1/ClientGUI.java
+++ code/simplechat1/ClientGUI.java
@@ -3,22 +3,67 @@ import java.awt.event.*;
 import java.awt.*;
 
 public class ClientGUI extends JFrame implements ActionListener {
+	// Variables that represent various components of the client
 	private JFrame mainProgram;
+	
+	// Menu bar and associated menus
 	private JMenuBar mb;
 	private JMenu file;
 	private JMenu help;
+	
+	// Menu items for the client
 	private JMenuItem about;
+	private JMenuItem logIn;
+	private JMenuItem logOut;
+	private JMenuItem exitProgram;
+	
+	// Variables for contact dialoog and log in
 	private JDialog contactInfo;
+	private JButton closeAbout;
+	private JDialog loginBox;
+	
+	// Sending message variables
+	private JTextField message;
+	private JLabel messageLabel;
+	private JButton sendButton;
+	
+	// Chat box and connection status
+	private JTextArea chatBox;
+	private JLabel connectionStatus, date;
 	
 	public ClientGUI() {
 		/* Name the program's frame and set its size */
 		mainProgram = new JFrame("SimpleChatClient");
-		mainProgram.setSize(500, 500);
+		mainProgram.setLayout(null);
+		mainProgram.setSize(630, 500);
 		
-		// Create a panel that will hold all other elements of the GUI
-		JPanel panel = new JPanel();
-		panel.setBounds(40, 80, 200, 200);
-		panel.setBackground(Color.gray);
+		// Add a label, text area, and button for sending messages
+		message = new JTextField();
+		message.setBounds(10,380,240,50);
+		
+		messageLabel = new JLabel("Type your message here: ");
+		messageLabel.setBounds(10,350,200,20);
+		
+		sendButton = new JButton("Send");
+		sendButton.setBounds(300,380,100,20);
+		
+		mainProgram.add(message);
+		mainProgram.add(messageLabel);
+		mainProgram.add(sendButton);
+		
+		// Add event listener for the sendButton
+		sendButton.addActionListener(this);
+		
+		// Add a chat box
+		chatBox = new JTextArea("\n");
+		chatBox.setBounds(10, 0, 300, 300);
+		chatBox.setEditable(true);
+		mainProgram.add(chatBox);
+		
+		// Add labels that indicate current date and connectivity status
+		connectionStatus = new JLabel("You're not logged in! To login, go to File->Login");
+		connectionStatus.setBounds(320,0,275,50);
+		mainProgram.add(connectionStatus);
 		
 		// Add a menu bar
 		mb = new JMenuBar();
@@ -31,6 +76,14 @@ public class ClientGUI extends JFrame implements ActionListener {
 		about = new JMenuItem("About");
 		help.add(about);
 		
+		// Add options for logging in, logging out and closing the program
+		logIn = new JMenuItem("Login");
+		logOut = new JMenuItem("Logout");
+		exitProgram = new JMenuItem("Exit");
+		file.add(logIn);
+		file.add(logOut);
+		file.add(exitProgram);
+		
 		// Create a dialog box for contact information and version history
 		contactInfo = new JDialog(mainProgram);
         contactInfo.setTitle("About SimpleChatGUI Client.");
@@ -39,8 +92,15 @@ public class ClientGUI extends JFrame implements ActionListener {
 		// Add elements to the dialog (description and button)
 		JPanel pan=new JPanel();
 		pan.setLayout(new FlowLayout());
-		pan.add(new JLabel("Simple Chat Client GUI is licensed under MIT license. Furthermore, OCSF component of Simple Chat was made at University of Ottawa"));
-		pan.add(new JButton("Close"));
+		pan.add(new JLabel("Simple Chat Client GUI is licensed under MIT license."));
+		pan.add(new JLabel("Furthermore, OCSF component of Simple Chat was made at University of Ottawa"));
+		
+		// Add action listener to the close button
+		closeAbout = new JButton("Close");
+		pan.add(closeAbout);
+		closeAbout.addActionListener(this);
+		
+		// Add the panel to the dialog
 		contactInfo.add(pan);
 		
 		// Location and visibility
@@ -50,12 +110,15 @@ public class ClientGUI extends JFrame implements ActionListener {
 			
 		// Add action listeners to the menu items
 		about.addActionListener(this);
+		logIn.addActionListener(this);
+		logOut.addActionListener(this);
+		exitProgram.addActionListener(this);
 
 		// Add menus to the menu bar. Then, add the menu and panel bar to the frame
 		mb.add(file);
 		mb.add(help);
 		mainProgram.setJMenuBar(mb);
-		mainProgram.add(panel);
+		//mainProgram.add(panel);
 		
 		// Operations for closing the client and its display
 		mainProgram.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -67,9 +130,29 @@ public class ClientGUI extends JFrame implements ActionListener {
 			// Display the dialog box
 			contactInfo.setVisible(true);
 		}
+		if (e.getSource()==closeAbout) {
+			// Close the box
+			contactInfo.setVisible(false);
+		}
+		if (e.getSource()==exitProgram) {
+			// Exit the program
+			System.exit(0);
+		}
+		if (e.getSource()==sendButton) {
+			// Send the message to the server
+			String out = message.getText();
+			System.out.println(out);
+			// Clean the message box and display the message into the chat box
+			message.setText(null);
+			chatBox.append(out+"\n");
+		}
 	}
 	
 	public static void main(String[] args) {
-		ClientGUI gui = new ClientGUI();
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new ClientGUI();
+			}
+		});
 	}
 }
